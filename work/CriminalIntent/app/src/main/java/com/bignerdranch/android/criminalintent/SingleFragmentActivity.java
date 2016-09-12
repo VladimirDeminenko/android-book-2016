246abcb948a5ce6ffc271be65ff9916c8a5ee81a
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2016-09-12 12:37:48 +0200

    Chapter 13. Tools Panel
     Changes to be committed:
    	modified:   CriminalIntent/app/src/main/AndroidManifest.xml
    	modified:   CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
    	modified:   CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
    	modified:   CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
    	modified:   CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
    	new file:   CriminalIntent/app/src/main/res/drawable-hdpi/ic_menu_add.png
    	new file:   CriminalIntent/app/src/main/res/drawable-mdpi/ic_menu_add.png
    	new file:   CriminalIntent/app/src/main/res/drawable-xhdpi/ic_menu_add.png
    	new file:   CriminalIntent/app/src/main/res/drawable-xxhdpi/ic_menu_add.png
    	new file:   CriminalIntent/app/src/main/res/menu/fragment_crime_list.xml
    	modified:   CriminalIntent/app/src/main/res/values/strings.xml

--- work/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ work/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -2,15 +2,15 @@ package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.UUID;
 
 /**
  * Created by Vladimir on 06.09.2016.
  */
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
     protected abstract Fragment createFragment();
 
     @Override
